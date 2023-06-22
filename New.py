0ea08de4a2ba944cbc0d080a33afe3b1f7f632d3
Sfertyser <a.klimenko0907@gmail.com>
2023-06-22 17:50:16 +0300

    Init commit

--- New.py
+++ New.py
@@ -1,8 +1,30 @@
-number = 10
-text = ""
+# Тернарний оператор
+# number = 10
+# text = ""
 # if number % 2 == 0:
 #     text = "парним"
 # else:
 #     text = "не парним"
-text = "парним" if number % 2 == 0 else "не парним"   # Тарнарний оператор
-print(f"{number} є {text}")
+# text = "парним" if number % 2 == 0 else "не парним"
+# print(f"{number} є {text}")
+
+# Робота над помилками
+# month = int(input("Введіть місяць: "))
+#   Через while not
+# while not 1 <= month <= 12:
+#     print("не вірний місяць")
+#     month = int(input("Введіть місяць: "))
+# print("Все вірно")
+# print(type(month))
+#   Через range
+# while month not in range(1, 13):
+#     print("не вірний місяць")
+#     month = int(input("Введіть місяць: "))
+# print("Все вірно")
+
+name = "Oleksandr"
+number = 13
+if False:
+    print(name)
+else:
+    print(number)
