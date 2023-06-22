3fee08946da0f01d5f1e14124ba16a86c5214ecb
Sfertyser <a.klimenko0907@gmail.com>
2023-06-22 23:53:06 +0300

    Init commit

--- Youtube.py
+++ Youtube.py
@@ -1,13 +1,27 @@
 # string methods
-name = "Oleksandr"
+# name = "oleksandr"
+# print(len(name))
+# print(name.find("k"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("o"))
+# print(name.replace("e","a"))
+# print(name*3)
 
-print(len(name))
-print(name.find("k"))
-print(name.capitalize())
-print(name.upper())
-print(name.lower())
-print(name.isdigit())
-print(name.isalpha())
-print(name.count("o"))
-print(name.replace("e","a"))
-print(name*3)
+# math functions
+import math
+# pi = 3.14
+# x = 1
+# y = 2
+# z = 3
+# print(round(pi)) rounds number.
+# print(math.ceil(pi)) rounds number up.
+# print(math.floor(pi)) rounds number down.
+# print(abs(pi)) turns negative number into positive.
+# print(pow(pi,2)) multiples first number by itself set amount of times.
+# print(math.sqrt(pi)) writes a square of number.
+# print(max(x,y,z)) writes max value of numbers from targeted.
+# print(min(x,y,z)) writes min value of numbers from targeted.
