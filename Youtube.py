b113c99d7c0672ee1ba1fc6ab096b1e74a83f2b1
Sfertyser <a.klimenko0907@gmail.com>
2023-06-22 23:06:04 +0300

    Init commit

---
+++ Youtube.py
@@ -0,0 +1,13 @@
+# string methods
+name = "Oleksandr"
+
+print(len(name))
+print(name.find("k"))
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("o"))
+print(name.replace("e","a"))
+print(name*3)
