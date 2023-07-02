4c6ae717c07532d2a9683f3bce4305c18a9f0ddb
Sfertyser <a.klimenko0907@gmail.com>
2023-07-02 11:58:45 +0300

    Init commit

--- Youtube.py
+++ Youtube.py
@@ -30,7 +30,7 @@
 # slicing = create a substring by extracting elements from another string
 #           indexing[] or slice()
 #           [start:stop:step]
-name = "Klimenko Oleksandr"
+# name = "Klimenko Oleksandr"
 # first_name = name[:8]
 # last_name = name[9:18]
 # funky_name = name[::2]
@@ -38,6 +38,6 @@ name = "Klimenko Oleksandr"
 
 website1 = "http://google.com"
 website2 = "http://wikipedia.com"
-slice = slice(7,-4)
+slice = slice(7, -4)
 print(website1[slice])
 print(website2[slice])
