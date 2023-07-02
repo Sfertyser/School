2e0cee4abb4416a0b02d928e4312bdb567f70d17
Sfertyser <a.klimenko0907@gmail.com>
2023-07-02 11:50:28 +0300

    Init commit

--- Youtube.py
+++ Youtube.py
@@ -12,7 +12,7 @@
 # print(name*3)
 
 # math functions
-import math
+# import math
 # pi = 3.14
 # x = 1
 # y = 2
@@ -25,3 +25,19 @@ import math
 # print(math.sqrt(pi)) writes a square of number.
 # print(max(x,y,z)) writes max value of numbers from targeted.
 # print(min(x,y,z)) writes min value of numbers from targeted.
+
+# string slicing
+# slicing = create a substring by extracting elements from another string
+#           indexing[] or slice()
+#           [start:stop:step]
+name = "Klimenko Oleksandr"
+# first_name = name[:8]
+# last_name = name[9:18]
+# funky_name = name[::2]
+# reverse_name = name[::-1]
+
+website1 = "http://google.com"
+website2 = "http://wikipedia.com"
+slice = slice(7,-4)
+print(website1[slice])
+print(website2[slice])
