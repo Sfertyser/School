8ec4a8a8069eb5a823abeed84bf9132361f27868
Sfertyser <a.klimenko0907@gmail.com>
2023-07-19 21:57:15 +0300

    Init commit

--- Task.py
+++ Task.py
@@ -41,29 +41,91 @@
 
 # Task 4
 # Ця програма переводить метри в іншу одиницю довжини.
-meters = input("Введіть кількість метрів: ")
-symbols = 0
-for char in meters:
-    if not (char.isdigit() or char == '.'):    # Перевіряє чи є в змінній щось окрім чисел або крапки.
-        print("Помилка, ви ввели щось окрім чисел або крапки.")
-        exit()
-    if char == ".":    # Перевіряє чи більше 1 крапки є в змінній.
-        symbols += 1
-        if symbols > 1:
-            print("Помилка, ви ввели більше 1 крапки.")
-            exit()
-if symbols == 0:    # Переводить змінну в інший тип данних за кількістю крапок.
-    meters = int(meters)
-else:
-    meters = float(meters)
-print("Варіанти: милі дюйми ярди.")
-user_select = input("В яку одиницю довжини перевести: ")
-match user_select:
-    case "милі":
-        print(f"З {meters} метрів виходить {meters * 0.000621371} миль.")
-    case "дюйми":
-        print(f"З {meters} метрів виходить {meters * 39.37} дюймів.")
-    case "ярди":
-        print(f"З {meters} метрів виходить {meters * 1.09361} ярдів.")
-    case _:
-        print("Помилка, ви ввели щось не так.")
+# meters = input("Введіть кількість метрів: ")
+# symbols = 0
+# for char in meters:
+#     if not (char.isdigit() or char == '.'):    # Перевіряє чи є в змінній щось окрім чисел або крапки.
+#         print("Помилка, ви ввели щось окрім чисел або крапки.")
+#         exit()
+#     if char == ".":    # Перевіряє чи більше 1 крапки є в змінній.
+#         symbols += 1
+#         if symbols > 1:
+#             print("Помилка, ви ввели більше 1 крапки.")
+#             exit()
+# if symbols == 0:    # Переводить змінну в інший тип данних за кількістю крапок.
+#     meters = int(meters)
+# else:
+#     meters = float(meters)
+# print("Варіанти: милі дюйми ярди.")
+# user_select = input("В яку одиницю довжини перевести: ")
+# match user_select:
+#     case "милі":
+#         print(f"З {meters} метрів виходить {meters * 0.000621371} миль.")
+#     case "дюйми":
+#         print(f"З {meters} метрів виходить {meters * 39.37} дюймів.")
+#     case "ярди":
+#         print(f"З {meters} метрів виходить {meters * 1.09361} ярдів.")
+#     case _:
+#         print("Помилка, ви ввели щось не так.")
+
+# Task 5
+# try:
+#     day = int(input("Введіть день тижня: "))
+#     match day:
+#         case 1:
+#             print("Понеділок")
+#         case 2:
+#             print("Вівторок")
+#         case 3:
+#             print("Середа")
+#         case 4:
+#             print("Четвер")
+#         case 5:
+#             print("П`ятниця")
+#         case 6:
+#             print("Субота")
+#         case 7:
+#             print("Неділя")
+#         case _:
+#             print("Помилка, ви ввели не те число.")
+# except ValueError as error:
+#     print("Помилка, ви ввели текст або символи.")
+# except Exception as error:
+#     print("Помилка!")
+
+# Task 6
+# try:
+#     num1 = float(input("Введіть перше число: "))
+#     num2 = float(input("Введіть друге число: "))
+#     if num1 == num2:
+#         print("Числа однакові.")
+#     else:
+#         min_num = min(num1, num2) # Шукає менше число
+#         max_num = max(num1, num2) # Шукає більше число
+#         print(f"Числа у порядку зростання:{min_num}, {max_num}")
+# except ValueError as error:
+#     print("Помилка, ви ввели щось окрім цілих або дробових чисел.")
+
+# Task 7
+try:
+    num1 = float(input("Введіть перше число: "))
+    num2 = float(input("Введіть друге число: "))
+    operation = input("Введіть математичну дію (+, -, *, /): ")
+    match operation:
+        case '+':
+            result = num1 + num2
+        case '-':
+            result = num1 - num2
+        case '*':
+            result = num1 * num2
+        case '/':
+            if num1 == 0 or num2 == 0:
+                print("Помилка, ділити на нуль не можна.")
+                exit()
+            else:
+                result = num1 / num2
+        case '_':
+            print("Помилка, ви ввели неправильну дію.")
+    print(f"Результат:{result}")
+except ValueError as error:
+    print("Помилка, введіть цілі або дробові числа.")
