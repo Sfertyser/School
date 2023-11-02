e1d20db93c695511d6c0962694bcbe077cfe61da
Sfertyser <a.klimenko0907@gmail.com>
2023-11-03 00:22:41 +0200

    Init commit

--- Archive.py
+++ Archive.py
@@ -1424,3 +1424,164 @@ count
 #     print("Помилка! Ділити на нуль не можна.")
 # except Exception:
 #     print("Помилка! Зв'яжіться з розробником програми.")
+
+#  Класи
+
+# class Cat:
+#     def init(self, name, breed):
+#         self.name = name
+#         self.breed = breed
+#
+# my_cat = Cat("Павло", "Дворянин")
+# print(my_cat.name)
+
+# Наслiдування
+
+# class Animal:
+#     def init(self, species):
+#         self.species = species
+#
+# class Cat(Animal):
+#     def init(self, name, breed):
+#         super().init("Cat")
+#         self.name = name
+#         self.breed = breed
+#
+# my_cat = Cat("Павло", "Дворянин")
+# print(my_cat.species)
+
+# class Animal:
+#     def speak(self):
+#         print("Тварина звучить")
+#
+# class Cat(Animal):
+#     def speak(self):
+#         super().speak()
+#         print("Павло мурчить")
+#
+# my_cat = Cat()
+# my_cat.speak()
+
+# Iнкапсуляцiя
+
+# class BankAccount:
+#     def init(self, balance):
+#         self.__balance = balance
+#
+#     def get_balance(self):
+#         return self.__balance
+#
+#     def deposit(self, amount):
+#         if amount > 0:
+#             self.__balance += amount
+#
+#     def withdraw(self, amount):
+#         if amount > 0 and amount <= self.__balance:
+#             self.__balance -= amount
+#
+# my_account = BankAccount(1000)
+# my_account.withdraw(100)
+# print(my_account.get_balance())
+
+#  Полiморфiзм
+
+# class Cat:
+#     def sound(self):
+#         print("Мяу")
+#
+# class Dog:
+#     def sound(self):
+#         print("Гав")
+#
+# def make_sound(animal):
+#     print(animal.sound())
+#
+# my_cat = Cat()
+# my_dog = Dog()
+#
+# make_sound(my_cat)
+# make_sound(my_dog)
+
+# task 1
+# class Student:
+#     def __init__(self, name, age, grade):
+#         self.__name = name
+#         self.__age = age
+#         self.__grade = grade
+#
+#     def name1(self):
+#         return self.__name
+#
+#     def age1(self):
+#         return self.__age
+#
+#     def grade1(self):
+#         return self.__grade
+#
+#     def set_grade(self, grade):
+#         if 0 <= grade <= 100:
+#             self.__grade = grade
+#         else:
+#             print("Некоректнно заданна оцінка.")
+#
+#
+# student1 = Student("Олександр", 25, 50)
+# print(student1.name1())
+# print(student1.age1())
+# print(student1.grade1())
+#
+# student1.set_grade(100)
+# print(student1.grade1())
+
+# Task 2
+# class Book:
+#     def __init__(self, name, creator):
+#         self.__name = name
+#         self.__creator = creator
+#
+#     def get_name(self):
+#         return self.__name
+#
+#     def get_creator(self):
+#         return self.__creator
+#
+#
+# book1 = Book("Пригоди", "Іван Франко")
+# print(book1.get_name())
+# print(book1.get_creator())
+
+# Task 3
+# class Car:
+#     def __init__(self, mark, model):
+#         self.__mark = mark
+#         self.__model = model
+#         self.__speed = 0
+#
+#     def get_mark(self):
+#         return self.__mark
+#
+#     def get_model(self):
+#         return self.__model
+#
+#     def get_speed(self):
+#         return self.__speed
+#
+#     def accelerate(self, amount):
+#         if amount > 0:
+#             self.__speed += amount
+#
+#     def brake(self, amount):
+#         if amount > 0:
+#             self.__speed -= amount
+#             if self.__speed < 0:
+#                 self.__speed = 0
+#
+#
+# new_car = Car("Ford", "Transit")
+# print(new_car.get_mark())
+# print(new_car.get_model())
+# print(new_car.get_speed())
+# new_car.accelerate(60)
+# print(new_car.get_speed())
+# new_car.brake(20)
+# print(new_car.get_speed())
--- Task.py
+++ Task.py
@@ -1,160 +1,142 @@
-#  Класи
+# Завдання 1: Створення класу для представлення бібліотеки з інкапсуляції.
+# Створіть клас Library, який представляє бібліотеку з каталогом книг та методами для додавання та видалення книг.
+# Приховати каталог книг із використанням інкапсуляції.
+class Library:
+    def __init__(self):
+        # Приватне поле каталогу книг, починається з пустого списку.
+        self.__catalog = []
 
-# class Cat:
-#     def init(self, name, breed):
-#         self.name = name
-#         self.breed = breed
-#
-# my_cat = Cat("Павло", "Дворянин")
-# print(my_cat.name)
+    def add_book(self, book):
+        # Метод для додавання книги до каталогу.
+        self.__catalog.append(book)
+        print(f"Книга '{book}' додана до каталогу.")
 
-# Наслiдування
+    def remove_book(self, book):
+        # Метод для видалення книги з каталогу.
+        if book in self.__catalog:
+            self.__catalog.remove(book)
+            print(f"Книга '{book}' видалена з каталогу.")
+        else:
+            print(f"Книга '{book}' не знайдена в каталозі.")
 
-# class Animal:
-#     def init(self, species):
-#         self.species = species
-#
-# class Cat(Animal):
-#     def init(self, name, breed):
-#         super().init("Cat")
-#         self.name = name
-#         self.breed = breed
-#
-# my_cat = Cat("Павло", "Дворянин")
-# print(my_cat.species)
+    def get_catalog(self):
+        # Метод для отримання списку всіх книг в каталозі (інформація про інкапсульований каталог).
+        return self.__catalog
 
-# class Animal:
-#     def speak(self):
-#         print("Тварина звучить")
-#
-# class Cat(Animal):
-#     def speak(self):
-#         super().speak()
-#         print("Павло мурчить")
-#
-# my_cat = Cat()
-# my_cat.speak()
 
-# Iнкапсуляцiя
+# Приклад використання класу Library:
+my_library = Library()
 
-# class BankAccount:
-#     def init(self, balance):
-#         self.__balance = balance
-#
-#     def get_balance(self):
-#         return self.__balance
-#
-#     def deposit(self, amount):
-#         if amount > 0:
-#             self.__balance += amount
-#
-#     def withdraw(self, amount):
-#         if amount > 0 and amount <= self.__balance:
-#             self.__balance -= amount
-#
-# my_account = BankAccount(1000)
-# my_account.withdraw(100)
-# print(my_account.get_balance())
+my_library.add_book("Книга 1")
+my_library.add_book("Книга 2")
+my_library.add_book("Книга 3")
 
-#  Полiморфiзм
+print("Каталог книг в бібліотеці:")
+print(my_library.get_catalog())
 
-# class Cat:
-#     def sound(self):
-#         print("Мяу")
-#
-# class Dog:
-#     def sound(self):
-#         print("Гав")
-#
-# def make_sound(animal):
-#     print(animal.sound())
-#
-# my_cat = Cat()
-# my_dog = Dog()
-#
-# make_sound(my_cat)
-# make_sound(my_dog)
+my_library.remove_book("Книга 2")
 
-# task 1
-# class Student:
-#     def __init__(self, name, age, grade):
-#         self.__name = name
-#         self.__age = age
-#         self.__grade = grade
-#
-#     def name1(self):
-#         return self.__name
-#
-#     def age1(self):
-#         return self.__age
-#
-#     def grade1(self):
-#         return self.__grade
-#
-#     def set_grade(self, grade):
-#         if 0 <= grade <= 100:
-#             self.__grade = grade
-#         else:
-#             print("Некоректнно заданна оцінка.")
-#
-#
-# student1 = Student("Олександр", 25, 50)
-# print(student1.name1())
-# print(student1.age1())
-# print(student1.grade1())
-#
-# student1.set_grade(100)
-# print(student1.grade1())
+print("Оновлений каталог книг в бібліотеці:")
+print(my_library.get_catalog())
 
-# Task 2
-# class Book:
-#     def __init__(self, name, creator):
-#         self.__name = name
-#         self.__creator = creator
-#
-#     def get_name(self):
-#         return self.__name
-#
-#     def get_creator(self):
-#         return self.__creator
-#
-#
-# book1 = Book("Пригоди", "Іван Франко")
-# print(book1.get_name())
-# print(book1.get_creator())
 
-# Task 3
-# class Car:
-#     def __init__(self, mark, model):
-#         self.__mark = mark
-#         self.__model = model
-#         self.__speed = 0
-#
-#     def get_mark(self):
-#         return self.__mark
-#
-#     def get_model(self):
-#         return self.__model
-#
-#     def get_speed(self):
-#         return self.__speed
-#
-#     def accelerate(self, amount):
-#         if amount > 0:
-#             self.__speed += amount
-#
-#     def brake(self, amount):
-#         if amount > 0:
-#             self.__speed -= amount
-#             if self.__speed < 0:
-#                 self.__speed = 0
-#
-#
-# new_car = Car("Ford", "Transit")
-# print(new_car.get_mark())
-# print(new_car.get_model())
-# print(new_car.get_speed())
-# new_car.accelerate(60)
-# print(new_car.get_speed())
-# new_car.brake(20)
-# print(new_car.get_speed())
+# Завдання 2: Створення класу подання співробітника з допомогою інкапсуляції.
+# Створіть клас Employee, який представляє співробітника з ім'ям, посадою та зарплатою.
+# Приховати дані про зарплату за допомогою інкапсуляції.
+
+
+class Employee:
+    def __init__(self, name, position, salary):
+        self.name = name
+        self.position = position
+        # Приватне поле для зарплати.
+        self.__salary = salary
+
+    def get_salary(self):
+        # Метод для отримання зарплати співробітника (інформація про інкапсульовану зарплату).
+        return self.__salary
+
+    def set_salary(self, new_salary):
+        # Метод для зміни зарплати співробітника (за необхідності).
+        if new_salary >= 0:
+            self.__salary = new_salary
+            print(f"Зарплата співробітника {self.name} оновлена: {new_salary}")
+        else:
+            print("Зарплата не може бути від'ємною.")
+
+
+# Приклад використання класу Employee:
+employee1 = Employee("Іван", "Менеджер", 50000)
+
+print(f"Ім'я: {employee1.name}")
+print(f"Посада: {employee1.position}")
+print(f"Зарплата: {employee1.get_salary()}")
+
+employee1.set_salary(55000)  # Зміна зарплати на 55000
+print(f"Оновлена зарплата: {employee1.get_salary()}")
+
+employee1.set_salary(-1000)  # Намагання встановити від'ємну зарплату
+
+
+# Завдання 3: Створення класу представлення магазину з допомогою інкапсуляції.
+# Створіть клас Store, який представляє магазин з асортиментом товарів та методами для додавання та видалення товарів.
+# Приховати асортименти товарів з використанням інкапсуляції.
+
+
+class Store:
+    def __init__(self):
+        # Приватне поле для асортименту товарів, починається з пустого словника.
+        self.__inventory = {}
+
+    def add_item(self, item, quantity):
+        # Метод для додавання товару до асортименту магазину.
+        if item in self.__inventory:
+            # Якщо товар вже є в асортименті, додати кількість до існуючого товару.
+            self.__inventory[item] += quantity
+        else:
+            # Якщо товар відсутній в асортименті, створити новий запис.
+            self.__inventory[item] = quantity
+        print(f"Додано {quantity} одиниць товару '{item}' до асортименту.")
+
+    def remove_item(self, item, quantity):
+        # Метод для видалення товару з асортименту магазину.
+        if item in self.__inventory and self.__inventory[item] >= quantity:
+            self.__inventory[item] -= quantity
+            if self.__inventory[item] == 0:
+                del self.__inventory[item]
+            print(f"Видалено {quantity} одиниць товару '{item}' з асортименту.")
+        else:
+            print(f"Товар '{item}' не знайдено в асортименті або недостатньо кількості.")
+
+    def get_inventory(self):
+        # Метод для отримання асортименту товарів (інформація про інкапсульований асортимент).
+        return self.__inventory
+
+
+# Приклад використання класу Store:
+my_store = Store()
+
+my_store.add_item("Футболка", 100)
+my_store.add_item("Джинси", 50)
+my_store.add_item("Кросівки", 30)
+
+print("Асортимент товарів у магазині:")
+print(my_store.get_inventory())
+
+my_store.remove_item("Футболка", 20)
+
+print("Оновлений асортимент товарів у магазині:")
+print(my_store.get_inventory())
+
+# Програма повинна вивести трикутник як на малюнку.
+# Програма отримує число - скільки вивести рядків.
+# Кожен рядок має номер та число вирівняне по правому краю з кількистю нулів відповіно до номера рядку.
+
+# Запит користувача про кількість рядків
+num_rows = int(input("Введіть кількість рядків: "))
+
+# Використовуємо цикл для створення трикутника
+for i in range(1, num_rows + 1):
+    # Створюємо рядок з номером та нулями, вирівняними по правому краю
+    row = f"{i:2d} {'0' * i}"
+    print(row)
