b81dec90abe15144a3e643b9b938ce3a58beffba
Sfertyser <a.klimenko0907@gmail.com>
2024-01-05 17:04:39 +0200

    Init commit

--- Archive.py
+++ Archive.py
@@ -2452,21 +2452,50 @@ timedelta має кілька атрибутів, які надають інфо
 """
 
 
-def verification(input_date_str):
-    try:
-        user_date = (datetime.strptime
-                     (input_date_str, "%Y-%m-%d %H:%M:%S"))
-        current_date = datetime.now()
-        next_date = current_date + timedelta(days=7)
-        if current_date <= user_date <= next_date:
-            print(f"Ваша дата та час: {user_date}, відповідають діапазону.")
-        else:
-            print(f"Ваша дата та час: {user_date}, не відповідають діапазону.")
-    except Exception as e:
-        print(f"Помилка!, {str(e)}")
+# def verification(input_date_str):
+#     try:
+#         user_date = (datetime.strptime
+#                      (input_date_str, "%Y-%m-%d %H:%M:%S"))
+#         current_date = datetime.now()
+#         next_date = current_date + timedelta(days=7)
+#         if current_date <= user_date <= next_date:
+#             print(f"Ваша дата та час: {user_date}, відповідають діапазону.")
+#         else:
+#             print(f"Ваша дата та час: {user_date}, не відповідають діапазону.")
+#     except Exception as e:
+#         print(f"Помилка!, {str(e)}")
+#
+#
+# user_date_time = input("Введіть вашу дату у форматі (YYYY-MM-DD "
+#                        "HH:MM:SS): ")
+# result = verification(user_date_time)
+# print(result)
+
+"""
+Розрахунок різниці між датами
+
+Напишіть програму, яка приймає введення користувача у вигляді двох дат в форматі
+"YYYY-MM-DD". Розрахуйте та виведіть на екран різницю в днях між цими датами.
+
+Вимоги до програми:
+
+1. Використайте бібліотеку datetime для роботи з датами.
+2. Запитуйте користувача про введення двох дат.
+3. Розрахуйте різницю між цими датами.
+4. Виведіть на екран кількість днів, яка розділяє ці дві дати.
+
+Обидві ці задачі дозволять вам використовувати бібліотеку datetime
+для роботи з часовими та датовими даними в Python.
+"""
 
 
-user_date_time = input("Введіть вашу дату у форматі (YYYY-MM-DD "
-                       "HH:MM:SS): ")
-result = verification(user_date_time)
-print(result)
+# def difference(input_date_str1, input_date_str2):
+#     user_date1 = (datetime.strptime(input_date_str1, "%Y-%m-%d"))
+#     user_date2 = (datetime.strptime(input_date_str2, "%Y-%m-%d"))
+#     days_left = abs((user_date2 - user_date1).days)
+#     print(f"Різниця між днями складає {days_left} днів.")
+#
+#
+# user_date1 = input("Введіть першу дату у форматі (YYYY-MM-DD): ")
+# user_date2 = input("Введіть другу дату у форматі (YYYY-MM-DD): ")
+# difference(user_date1, user_date2)
