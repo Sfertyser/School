3c518730ccc9f4f8009f07410bdfa495dc7853c4
Sfertyser <a.klimenko0907@gmail.com>
2024-01-27 12:38:02 +0200

    Init commit

--- Archive.py
+++ Archive.py
@@ -2621,3 +2621,23 @@ timedelta має кілька атрибутів, які надають інфо
 #     print(f"{str(e)}, перевірте правильність введених даних.")
 # except Exception as e:
 #     print(f"Виникла помилка: {str(e)}")
+
+import requests
+""" Відправте GET-запит до веб-сайту
+ та виведіть статус відповіді (код статусу) """
+# url = "https://www.example.com"
+# response = requests.get(url)
+# print(response)
+
+""" Відправте POST-запит до веб-сервера
+ з певними даними та виведіть вміст відповіді. """
+# url = "https://www.example.com"
+# data = {'key': 'value'}
+# response = requests.post(url, data=data)
+# print(response.text)
+
+""" Запитайте користувача про URL-адресу,
+ а потім відправте GET-запит за введеним URL. """
+# url = input("Введіть URL-адресу: ")
+# response = requests.get(url)
+# print(response)
