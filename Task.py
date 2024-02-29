21977e7dd5b178dc1b1bf01507f027318087cbe0
Sfertyser <a.klimenko0907@gmail.com>
2024-02-29 21:33:16 +0200

    Init commit

--- Task.py
+++ Task.py
@@ -8,10 +8,10 @@ import datetime
 4 Згенерувати випадкову послідовність букв заданої довжини.
 5 Випадковим чином обрати пароль заданої довжини зі списку допустимих символів.
 6 Розіграти лотерею, вибираючи випадковий набір чисел з певного діапазону.
-Симулювати кидання грального кубика (генерувати випадкове число від 1 до 6).
-Випадковим чином розподілити учасників на команди для змагань.
-Згенерувати випадкову дату в певному діапазоні.
-Випадковим чином змінювати порядок рядків у двовимірному списку (матриці).
+7 Симулювати кидання грального кубика (генерувати випадкове число від 1 до 6).
+8 Випадковим чином розподілити учасників на команди для змагань.
+9 Згенерувати випадкову дату в певному діапазоні.
+10 Випадковим чином змінювати порядок рядків у двовимірному списку (матриці).
 """
 # 1
 # random_num = random.randint(1, 100)
@@ -54,9 +54,61 @@ import datetime
 
 # 8
 """Випадковим чином розподілити учасників на команди для змагань."""
-players = ["Максим", "Денис", "Назар", "Дмитро"]
-num_teams = 2
-random.shuffle(players)
-teams = [players[i::num_teams] for i in range(num_teams)]
-for i, team in enumerate(teams, start=1):
-    print(f"Команда {i}: {team}")
+# V1
+# players = ["Максим", "Денис", "Назар", "Дмитро"]
+# num_teams = 2
+# random.shuffle(players)
+# teams = [players[i::num_teams] for i in range(num_teams)]
+# for i, team in enumerate(teams, start=1):
+#     print(f"Команда {i}: {team}")
+
+# V2
+# players = ["Максим", "Денис", "Назар", "Дмитро"]
+# num_teams = 2
+# teams = [[] for _ in range(num_teams)]
+# while players:
+#     for i in range(num_teams):
+#         if players:
+#             random_index = random.randint(0, len(players) - 1)
+#             teams[i].append(players.pop(random_index))
+#
+# print("Команди для змагань:")
+# for i, team in enumerate(teams, 1):
+#     print(f"Команда {i}: {', '.join(team)}")
+
+# 9
+"""Згенерувати випадкову дату в певному діапазоні."""
+# start_date = datetime.date(2000, 1, 1)
+# end_date = datetime.date(2024, 12, 31)
+#
+# random_days = (end_date - start_date).days
+# random_day_offset = random.randint(0, random_days)
+# random_date = start_date + datetime.timedelta(days=random_day_offset)
+#
+# print(f"Випадкова дата: {random_date}")
+
+# 10
+"""Випадковим чином змінювати порядок рядків у двовимірному списку (матриці)."""
+
+
+# def shuffle_matrix(matrix):
+#     random.shuffle(matrix)
+#     return matrix
+#
+#
+# matrix = [
+#          [1, 2, 3],
+#          [4, 5, 6],
+#          [7, 8, 9]
+#          ]
+#
+# print("Початкова матриця:")
+# for row in matrix:
+#     print(row)
+#
+# shuffled_matrix = shuffle_matrix(matrix)
+#
+# print("\nМатриця після випадкового перетасування рядків:")
+# for row in shuffled_matrix:
+#     print(row)
+#
