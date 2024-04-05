08fe27485e535bb9132a14ee0c999989a27ae326
Sfertyser <a.klimenko0907@gmail.com>
2024-04-05 16:13:17 +0300

    Init commit

--- Task.py
+++ Task.py
@@ -8,20 +8,20 @@ import string
 # а кратні 3 і 5 - на "FizzBuzz".
 
 
-# def fizzbuzz(n):
-#     for i in range(1, n + 1):
-#         if i % 3 == 0 and i % 5 == 0:
-#             print("FizzBuzz")
-#         elif i % 3 == 0:
-#             print("Fizz")
-#         elif i % 5 == 0:
-#             print("Buzz")
-#         else:
-#             print(i)
-#
-#
-# n = 15
-# fizzbuzz(n)
+def fizzbuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+n = 15
+fizzbuzz(n)
 
 
 # 2
